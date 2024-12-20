5b290de3df8d814c5f9b817c8a2fc99267533528
navadenis <nvadns06e24d416i@iisbadoni.edu.it>
2024-12-20 19:07:23 +0100

    New Test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,15 +7,15 @@ class CalculatorTest {
 
     @Test
     void sum() {
-        Calculator calculator = new Calculator(3, 5);
+        Calculator calculator = new Calculator(2, 6);
         int result = calculator.sum();
         assertEquals(8, result, "Sum of 3 and 5 should be 8");
     }
 
     @Test
     void exp() {
-        Calculator calculator = new Calculator(2, 3);
+        Calculator calculator = new Calculator(2, 4);
         int result = calculator.exp();
-        assertEquals(8, result, "2 raised to the power of 3 should be 8");
+        assertEquals(16, result, "2 raised to the power of 3 should be 8");
     }
 }
